b190bd6784d6bc41a4171e551c0a378d3d1ff0d6
Jose María Ruano Gálvez <jruanogalvez@gmail.com>
2018-02-21 08:02:44 +0100

    Make text size fit to the with of the rectangle, added comments, 	refactorized code.
    	modified:   .idea/modules.xml
    	modified:   app/src/main/java/com/github/jruanodev/facedetector/FaceDetectorActivity.java
    	modified:   app/src/main/java/com/github/jruanodev/facedetector/FaceDetectorResultActivity.java

--- app/src/main/java/com/github/jruanodev/facedetector/FaceDetectorActivity.java
+++ app/src/main/java/com/github/jruanodev/facedetector/FaceDetectorActivity.java
@@ -12,6 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * @author Jose Ruano
+ */
+
 public class FaceDetectorActivity extends AppCompatActivity implements View.OnClickListener{
     Button btnChooseGallery;
 
--- app/src/main/java/com/github/jruanodev/facedetector/FaceDetectorResultActivity.java
+++ app/src/main/java/com/github/jruanodev/facedetector/FaceDetectorResultActivity.java
@@ -24,6 +24,10 @@ import com.google.android.gms.vision.face.FaceDetector;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
+/**
+ * @author Jose Ruano
+ */
+
 public class FaceDetectorResultActivity extends AppCompatActivity {
     ImageView resultImageView;
     ImageButton btnBack;
@@ -49,16 +53,23 @@ public class FaceDetectorResultActivity extends AppCompatActivity {
         Intent comingIntent = getIntent();
         image = BitmapFactory.decodeFile(comingIntent.getStringExtra("image"));
 
+        analyzeImage();
+    }
+
+    public void analyzeImage() {
+
+        // New paint with no fill no draw the rectangle
         Paint paint = new Paint();
         paint.setStrokeWidth(5);
         paint.setColor(Color.GREEN);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setTextSize(30);
 
+        // Create a temporal Bitmap of the same size of the original
         Bitmap tempBitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.RGB_565);
         Canvas canvas = new Canvas(tempBitmap);
         canvas.drawBitmap(image, 0, 0, null);
 
+        // Instantiate the FaceDetector Object (tracking ENABLED to use with getId())
         FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext())
                 .setTrackingEnabled(true)
                 .setLandmarkType(FaceDetector.ALL_LANDMARKS)
@@ -66,38 +77,63 @@ public class FaceDetectorResultActivity extends AppCompatActivity {
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
                 .build();
 
+        // Check if the detector is available
         if(faceDetector.isOperational()) {
+
+            // Create new frame and set the original bitmap
             Frame frame = new Frame.Builder().setBitmap(image).build();
+
+            // Get all the detected faces
             SparseArray<Face> faceSparseArray = faceDetector.detect(frame);
+
+            // Make a list to storage each one of the face property
             ArrayList<String> detailsArrayList = new ArrayList<>();
 
-            for(int i = 0; i < faceSparseArray.size(); i++) {
+            for (int i = 0; i < faceSparseArray.size(); i++) {
                 Face face = faceSparseArray.valueAt(i);
 
+                // Format the probability and populate the list with it
                 DecimalFormat decimalFormat = new DecimalFormat("##.#");
                 float probability = Float.valueOf(decimalFormat.format(face.getIsSmilingProbability() * 100));
-                if(face.getIsSmilingProbability() == -1.0)
+                if (face.getIsSmilingProbability() == -1.0)
+                    // If smile is not recognized display Unknown
                     detailsArrayList.add("Smiling probabilty of face " + (face.getId()) + ": Unknown");
                 else
                     detailsArrayList.add("Smiling probabilty of face " + (face.getId()) + ": " + probability + " %");
 
+                // Get the bounds of the face to draw the rectangle
                 float x1 = face.getPosition().x;
                 float y1 = face.getPosition().y;
                 float x2 = x1 + face.getWidth();
                 float y2 = y1 + face.getHeight();
 
+                // Make the rectangle with the desired bounds and draw it to a Canvas
                 RectF rectF = new RectF(x1, y1, x2, y2);
                 canvas.drawRoundRect(rectF, 2, 2, paint);
-                canvas.drawText("" + face.getId(), rectF.centerX(), rectF.centerY(), paint);
+
+                // Create new paint with fill to draw the text
+                Paint textPaint = new Paint();
+                textPaint.setStyle(Paint.Style.FILL);
+                textPaint.setColor(Color.GREEN);
+
+                // Calculate the text size depending on the with of the rectangle
+                int textSize = 0;
+                while (textPaint.measureText("Hola") < rectF.width()) {
+                    textPaint.setTextSize(++textSize);
+                }
+
+                // Draw the text inside each rectangle
+                canvas.drawText("" + face.getId(), rectF.centerX(), rectF.centerY(), textPaint);
 
             }
 
+            // Make a ArrayAdapter and populate the ListView with the data
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_list_item_1, detailsArrayList);
             detailsListView.setAdapter(adapter);
 
+            // Set the final Bitmap on the ImageView
             resultImageView.setImageBitmap(tempBitmap);
         }
-
     }
 }
